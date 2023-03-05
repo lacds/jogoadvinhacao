ce2bc20b3aabf2f2f2c99ce470eab9b67931fd20
lacds <126973950+lacds@users.noreply.github.com>
2023-03-05 13:21:02 -0300

    Pontuação
    Inserida pontuação final.

--- jogoadivinhacao.c
+++ jogoadivinhacao.c
@@ -1,6 +1,6 @@
 #include <stdio.h> //biblioteca padrão de entradas e saídas - responsável pela comunicação com o usuário
 #include <stdlib.h> //biblioteca responsável pela alocação de espaço e memória
-#include <locale.h> //biblioeca tipografia regional
+#include <locale.h> //biblioteca tipografia regional
 #include <string.h> 
 
 int main()
@@ -20,6 +20,7 @@ int main()
 	
 	int tentativas = 1;
 	
+	int pontos = 1000; //pontuação do jogo
 	
 	while(1)
 	{
@@ -33,7 +34,7 @@ int main()
 		if(chute<0)
 		{
 			printf("\n\nNúmeros negativos não valem! Tente novamente.\n");
-			continue; //continue faz ir direto para o i++ - para execução desse trecho e deixa o for continuar os demais trechos
+			continue; //faz ir direto para o i++ - para execução desse trecho e deixa o for continuar os demais trechos
 		}
 			
 		int acertou = (chute == numerosecreto);
@@ -48,25 +49,27 @@ int main()
 		}
 		else if(maior)
 		{
-			printf("\n\nAh, que pena, você errou!\n\n");
-			system("cls"); 
+			printf("\n\n\nAh, que pena, você errou!\n\n");
 			printf("\nVou te dar uma dica!\n");
 			printf("\nO número que você digitou é maior do que o número secreto...\n"); 
 		}
 		else
 		{
-			printf("\n\nAh, que pena, você errou!\n\n");
-			system("cls"); 
+			printf("\n\n\nAh, que pena, você errou!\n\n");
 			printf("\nVou te dar uma dica!\n");
 			printf("\nO número que você digitou é menor do que o número secreto...\n");
 		}
 		
 		tentativas++;
 		
+		int pontosperdidos = (chute - numerosecreto) / 2; //regra de pontuação
+		pontos = pontos - pontosperdidos;
+	
 	}
 	
 	printf("\n\nFim de jogo\n\n");
 	printf("\n\nVocê acertou em %d tentativas!\n\n", tentativas);
+	printf("\n\nTotal de pontos: %d\n\n", pontos);
 	
 }
 	
